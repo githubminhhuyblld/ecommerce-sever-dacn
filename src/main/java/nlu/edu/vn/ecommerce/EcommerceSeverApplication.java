de510016d3949e05ca7600cca41a73e868acf98a
Vũ Văn Minh <104952062+MellowSadBoy@users.noreply.github.com>
2023-06-23 15:49:20 +0700

    update deploy

--- src/main/java/nlu/edu/vn/ecommerce/EcommerceSeverApplication.java
+++ src/main/java/nlu/edu/vn/ecommerce/EcommerceSeverApplication.java
@@ -2,12 +2,13 @@ package nlu.edu.vn.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class EcommerceSeverApplication {
+public class EcommerceSeverApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceSeverApplication.class, args);
